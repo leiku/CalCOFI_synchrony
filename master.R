df311ff3655901b6353f2fba27d385ef74770eb8
Dan Reuman <reuman@ku.edu>
2018-04-05 20:44:14 -0500

    added master file I should have added at last commit

---
+++ master.R
@@ -0,0 +1,22 @@
+#This is just for running the different "main" files in the right order,
+#thereby producing all the results.
+#
+#Lei Zhao
+#_________
+
+#D: Lei, pls insert csvs downloaded from Calcofi, or send to Dan
+
+#***load the raw data and ... D: fill in this comment
+
+#D: source here some script that progresses from the raw csvs downloaded from Calcofi to the next stage of the data
+#(i.e. Data_all_seasons_interpolate.RDS). So after this line is run 
+
+#***detrend
+#D: source the script here that does the detrending, add to repo if not there already
+
+
+#***average to get time series for shallow and deep, also detrend
+#D: source the script here that does this
+
+#***
+
